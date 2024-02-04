7534167df5914aeced13c2b2d8ef91932a099961
doky <48897815+iamdoky@users.noreply.github.com>
2024-02-04 21:43:57 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        
+// )
+  
+        return my_string
+            .replace("a","")
+            .replace("e","")
+            .replace("i","")
+            .replace("o","")
+            .replace("u","");
+    }
+}
