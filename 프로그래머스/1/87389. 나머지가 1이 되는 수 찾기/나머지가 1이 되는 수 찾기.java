5f22b386394b08d9931e88d237ad5ecf49f1ce8d
doky <48897815+iamdoky@users.noreply.github.com>
2024-02-13 22:10:04 +0900

    Update 나머지가 1이 되는 수 찾기.java

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,16 +1,9 @@
 class Solution {
     public int solution(int n) {
-            
-		int answer = 0;
+	int answer = 0;
         
-        for (int i = 2; i < n; i++) {
-			
-        	if(n%i == 1) {
-        		answer = i;
-                
-                return answer;
-        	}	
-		}
+        for (int i = 2; i < n; i++)
+	    if (n % i == 1)return i;
         
         return answer;
     }
