4cd590646b6d94d4c464b8ab7639bb04b8d5a391
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-20 22:54:31 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.62 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,18 +1,25 @@
 
 import java.util.Arrays;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String str) {
         
-		String[] arr = str.replaceAll("[^0-9]", "").split("");
-		int[] answer = new int[arr.length];
+// 		String[] arr = str.replaceAll("[^0-9]", "").split("");
+// 		int[] answer = new int[arr.length];
         
-        Arrays.sort(arr);
+//         Arrays.sort(arr);
 		
-		for (int i = 0; i < answer.length; i++)
-            answer[i] = Integer.parseInt(arr[i]);
+// 		for (int i = 0; i < answer.length; i++)
+//             answer[i] = Integer.parseInt(arr[i]);
 		
         
-        return answer;
+//         return answer;
+        
+        
+        
+        
+        // return Arrays.stream(myString.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray()
+        return Arrays.stream(str.replaceAll("[^0-9]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
