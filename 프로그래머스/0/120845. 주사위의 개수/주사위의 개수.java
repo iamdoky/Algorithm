f36cc029d2cb7ba4a081e88ac143d4e131c8a994
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-20 22:09:22 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -2,10 +2,6 @@
 class Solution {
     public int solution(int[] box, int n) {
         
-        int a = box[0] / n;
-        int b = box[1] / n;
-        int c = box[2] / n;
-
-        return a * b * c;
+        return (box[0] / n) * (box[1] / n) * (box[2] / n);
     }
 }
