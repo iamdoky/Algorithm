91c847a2a0f972a5995152450345b7906d23a7af
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-20 21:50:42 +0900

    [level 0] Title: 가위 바위 보, Time: 1.92 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String rsp) {
-        return rsp.chars()
-                    .mapToObj(c -> {
-                        if (c == '0') return "5";
-                        else if (c == '2') return "0";
-                        else return "2";
-                  })
-                .collect(Collectors.joining());
+        
+        
+        return Arrays.stream(rsp.split(""))
+            .map(s -> s.equals("2") ? "0" : s.equals("0") ? "5" : "2")
+            .collect(Collectors.joining());
+        
+        
+        // return rsp.chars()
+        //             .mapToObj(c -> {
+        //                 if (c == '0') return "5";
+        //                 else if (c == '2') return "0";
+        //                 else return "2";
+        //           })
+        //         .collect(Collectors.joining());
     }
 }
