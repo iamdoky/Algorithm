92e8debde0bf2debf43a3061dbcaf8a32a3122ce
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-20 18:17:12 +0900

    [level 0] Title: 순서쌍의 개수, Time: 1.68 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;  
+
+class Solution {
+    public int solution(int n) {
+        return (int) IntStream.rangeClosed(1, (int) Math.sqrt(n))
+                              .filter(a -> n % a == 0)
+                              .map(a -> (a == n / a) ? 1 : 2)
+                              .sum();
+    }
+}
