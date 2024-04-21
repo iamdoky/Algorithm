1ce55762cc4368fb847195b94d071a46e5a66e76
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-21 18:17:07 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -4,23 +4,26 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
     
-        int arr1Length = arr1.length;
-        int arr2Length = arr2.length;
+//         int arr1Length = arr1.length;
+//         int arr2Length = arr2.length;
+//         int arr1Sum = Arrays.stream(arr1).sum();
+//         int arr2Sum = Arrays.stream(arr2).sum();
         
-        if(arr1Length == arr2Length) {
-            int arr1Sum = Arrays.stream(arr1).sum();
-            int arr2Sum = Arrays.stream(arr2).sum();
+//         if(arr1Length == arr2Length) {
+//             if (arr1Sum == arr2Sum) return 0;
             
-            if (arr1Sum == arr2Sum) {
-                return 0;
-            }
-            
-            return arr1Sum > arr2Sum ? 1 : -1;
-        }
+//             return arr1Sum > arr2Sum ? 1 : -1;
+//         }
         
-        return arr1Length > arr2Length ? 1 : -1;
+//         return arr1Length > arr2Length ? 1 : -1;
+        
+    
+        int answer = Integer.compare(arr1.length, arr2.length);
 
+        if(answer == 0)
+            return Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
 
-
+        return answer;
     }
+    
 }
