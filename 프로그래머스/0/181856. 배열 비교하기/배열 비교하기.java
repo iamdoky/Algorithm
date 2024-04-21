2888d96030a8472797482b66111f0c123b95e87d
doky <48897815+iamdoky@users.noreply.github.com>
2024-04-21 18:12:18 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,23 @@
+import java.util.stream.*;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+    
+        int arr1Length = arr1.length;
+        int arr2Length = arr2.length;
+        
+        if(arr1Length == arr2Length) {
+            int arr1Sum = Arrays.stream(arr1).sum();
+            int arr2Sum = Arrays.stream(arr2).sum();
+            
+            if (arr1Sum == arr2Sum) {
+                return 0;
+            }
+            
+            return arr1Sum > arr2Sum ? 1 : -1;
+        }
+        
+        return arr1Length > arr2Length ? 1 : -1;
+    }
+}
